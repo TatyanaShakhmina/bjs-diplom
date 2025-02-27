6fb617743f31c0d15201478828c581647282a5ea
TatyanaShakhmina <novichkovataty@gmail.com>
2025-02-27 18:09:58 +0400

    Вторая доработка

--- public/homePage.js
+++ public/homePage.js
@@ -1,15 +1,15 @@
 "use strict";
 const logoutButton = new LogoutButton();
 logoutButton.action = () => {
-    ApiConnector.logout( (response) => {
+    ApiConnector.logout(  (response) => {
         if (response.success) {
             location.reload();
         }
     })
 };
-ApiConnector.current( (response) => {
+ApiConnector.current(  (response) => {
     if (response.success) {
-        ProfileWidget.showProfile(response);
+        ProfileWidget.showProfile(response.data);
     }
 });
 
@@ -18,12 +18,74 @@ function getExchangeRates() {
     ApiConnector.getStocks( (response) => {
         if (response.success) {
             ratesBoard.clearTable();
-            ratesBoard.fillTable(data);
+            ratesBoard.fillTable(response.data);
         }
     });
 }
 getExchangeRates();
 setInterval(getExchangeRates, 60000);
 
+const moneyManager = new MoneyManager();
+moneyManager.addMoneyCallback = ({ currency, amount }) => {
+    ApiConnector.addMoney({ currency, amount }, (response) => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.message);
+        } else {
+            moneyManager.setMessage(response.message);
+        }
+    });
+}
+moneyManager.conversionMoneyCallback = ({ fromCurrency, targetCurrency, fromAmount }) => {
+    ApiConnector.convertMoney({ fromCurrency, targetCurrency, fromAmount }, (response) => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.message);
+        } else {
+            moneyManager.setMessage(response.message);
+        }
+    });
+}
+moneyManager.sendMoneyCallback = ({ to, currency, amount }) => {
+    ApiConnector.transferMoney({ to, currency, amount }, (response) => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.message);
+        } else {
+            moneyManager.setMessage(response.message);
+        }
+    });
+}
 
-
+const favoritesWidget = new FavoritesWidget();
+ApiConnector.getFavorites( (response) => {
+    if (response.success) {
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(response.data);
+        moneyManager.updateUsersList(response.data)
+    }
+});
+favoritesWidget.addUserCallback = ({ id, name }) => {
+    ApiConnector.addUserToFavorites({ id, name }, (response) => {
+        if (response.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data)
+            favoritesWidget.setMessage(response.message);
+        } else {
+            favoritesWidget.setMessage(response.message);
+        }
+    });
+}
+favoritesWidget.removeUserCallback = (id) => {
+    ApiConnector.removeUserFromFavorites(id, (response) => {
+        if (response.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data)
+            favoritesWidget.setMessage(response.message);
+        } else {
+            favoritesWidget.setMessage(response.message);
+        }
+    });
+}
--- public/loginPage.js
+++ public/loginPage.js
@@ -1,24 +1,20 @@
 "use strict";
 const userForm = new UserForm();
-userForm.loginFormCallback = (data) => {
-    console.log(data);
-    ApiConnector.login(data, (response) => {
-        console.log(response);
+userForm.loginFormCallback = ({ login, password }) => {
+    ApiConnector.login({ login, password }, (response) => {
         if (response.success) {
             location.reload();
         } else {
-            userForm.setLoginErrorMessage("Неверный логин или пароль");
+            userForm.setLoginErrorMessage(response.message);
         }
     })
 };
-userForm.registerFormCallback = (data) => {
-    console.log(data);
-    ApiConnector.login(data, (response) => {
-        console.log(response);
+userForm.registerFormCallback = ({ login, password }) => {
+    ApiConnector.register({ login, password }, (response) => {
         if (response.success) {
             location.reload();
         } else {
-            userForm.setRegisterErrorMessage("Ошибка");
+            userForm.setRegisterErrorMessage(response.message);
         }
     })
 };
