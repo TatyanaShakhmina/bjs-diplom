9670e57ec8e803e48069b585d0700024e44c6a78
TatyanaShakhmina <novichkovataty@gmail.com>
2025-03-03 12:28:54 +0400

    Третья доработка

--- public/homePage.js
+++ public/homePage.js
@@ -28,32 +28,38 @@ setInterval(getExchangeRates, 60000);
 const moneyManager = new MoneyManager();
 moneyManager.addMoneyCallback = ({ currency, amount }) => {
     ApiConnector.addMoney({ currency, amount }, (response) => {
+        let message = "";
         if (response.success) {
             ProfileWidget.showProfile(response.data);
-            moneyManager.setMessage(response.message);
+            message = "пополнение успешно";
         } else {
-            moneyManager.setMessage(response.message);
+            message = "ошибка при пополнении";
         }
+        moneyManager.setMessage(response.success, message);
     });
 }
 moneyManager.conversionMoneyCallback = ({ fromCurrency, targetCurrency, fromAmount }) => {
     ApiConnector.convertMoney({ fromCurrency, targetCurrency, fromAmount }, (response) => {
+        let message = "";
         if (response.success) {
             ProfileWidget.showProfile(response.data);
-            moneyManager.setMessage(response.message);
+            message = "конвертация успешна";
         } else {
-            moneyManager.setMessage(response.message);
+            message = "ошибка при конвертации";
         }
+        moneyManager.setMessage(response.success, message);
     });
 }
 moneyManager.sendMoneyCallback = ({ to, currency, amount }) => {
     ApiConnector.transferMoney({ to, currency, amount }, (response) => {
+        let message = "";
         if (response.success) {
             ProfileWidget.showProfile(response.data);
-            moneyManager.setMessage(response.message);
+            message = "перевод успешный";
         } else {
-            moneyManager.setMessage(response.message);
+            message = "ошибка при переводе";
         }
+        moneyManager.setMessage(response.success, message);
     });
 }
 
@@ -67,25 +73,29 @@ ApiConnector.getFavorites( (response) => {
 });
 favoritesWidget.addUserCallback = ({ id, name }) => {
     ApiConnector.addUserToFavorites({ id, name }, (response) => {
+        let message = "";
         if (response.success) {
             favoritesWidget.clearTable();
             favoritesWidget.fillTable(response.data);
-            moneyManager.updateUsersList(response.data)
-            favoritesWidget.setMessage(response.message);
+            moneyManager.updateUsersList(response.data);
+            message = "добавление успешно";
         } else {
-            favoritesWidget.setMessage(response.message);
+            message = "ошибка при добавлении";
         }
+        favoritesWidget.setMessage(response.success, message);
     });
 }
 favoritesWidget.removeUserCallback = (id) => {
     ApiConnector.removeUserFromFavorites(id, (response) => {
+        let message = "";
         if (response.success) {
             favoritesWidget.clearTable();
             favoritesWidget.fillTable(response.data);
-            moneyManager.updateUsersList(response.data)
-            favoritesWidget.setMessage(response.message);
+            moneyManager.updateUsersList(response.data);
+            message = "удаление успешно";
         } else {
-            favoritesWidget.setMessage(response.message);
+            message = "ошибка при удалении";
         }
+        favoritesWidget.setMessage(response.success, message);
     });
 }
